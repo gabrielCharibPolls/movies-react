49f1c9ebe2af0978c9b4aef9ab99c5eca234c22d
Gabriel Charib <gabriel.charibpolls@helb-prigogine.be>
2023-01-28 22:33:09 +0100

    creation of the menu compotent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import './css/app.css';
 import Home from './components/pages/Home';
+import Menu from './components/Menu';
 
 function App() {
   return (
+    <>
+   
+    <Menu/>
     <Home/>  
+    </>
   )
 }
 
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+   export default  function Menu(){
+     return (
+        <div className="menu">
+            <ul>
+                <li>Home</li>
+                <li>ADD techno</li>
+                <li>all tecno</li> 
+            </ul>
+        </div>
+     )
+   }
