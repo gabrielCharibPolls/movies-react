1bb2716cd9f9097371318d05bf5e6fccaeb47576
Gabriel Charib <gabriel.charibpolls@helb-prigogine.be>
2023-01-28 22:09:41 +0100

    creation of the home compotent

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+/* 
+creation d'un components 
+avec une fonction 
+*/
+
+export default function Home(){
+
+    return (
+        <div className="home">
+
+             <h1> My progretion</h1>
+             <div>welkom</div>
+        </div>
+       
+    )
+}
